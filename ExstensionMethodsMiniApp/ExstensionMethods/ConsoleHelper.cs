70ea8424289d954df1b278d30ffd553f30662aa0
Shah Haque <93956445+Shah-Haque@users.noreply.github.com>
2023-04-25 15:28:59 +0100

    Add files via upload

---
+++ ExstensionMethodsMiniApp/ExstensionMethods/ConsoleHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+            while (string.IsNullOrWhiteSpace(output))
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static int RequestInt (this string message, int minvalue, int maxvalue)
+        {
+            return message.RequestInt(true,minvalue, maxvalue);
+        }
+
+
+        public static int RequestInt(this string message)
+        {
+          return message.RequestInt(false);
+        }
+
+        private static int RequestInt(this string message,bool usedMinMax,
+                                        int minvalue = 0, int maxvalue = 0)
+        {
+            int output = 0;
+            bool isValidInt = false;
+            bool isInValidRange = true;
+
+            while (isValidInt == false || isInValidRange == false)
+            {
+                Console.Write(message);
+                isValidInt = int.TryParse(Console.ReadLine(), out output);
+
+                if (usedMinMax == true)
+                {
+                    if (output >= minvalue && output <= maxvalue )
+                    {
+                        isInValidRange = true;
+                    }
+                    else
+                    {
+                        isInValidRange = false;
+                    }
+                }
+            }
+            return output;
+        }
+
+    }
+}
---
+++ ExstensionMethodsMiniApp/ExstensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel();
+
+            p.FirstName = "what is your FirstName:  ".RequestString();
+           
+            p.LastName = "What is your LastName: ".RequestString();
+
+            p.Age = "What is your Age: " .RequestInt(0,120);
+
+            Console.ReadLine();
+        }
+    }
+}
